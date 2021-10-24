245bf192fcc3b0e7e02167e43de4111d30c17c4e
moacirandretti <moacir.andretti@gmail.com>
2021-10-23 23:51:57 -0300

    chore: altera dangerfile

--- dangerfile.ts
+++ dangerfile.ts
@@ -5,3 +5,10 @@ const routeFile = danger.git.fileMatch('src/routes.ts')
 if (routeFile.edited) {
   fail('Você alterou o arquivo de rotas')
 }
+
+danger.git.JSONDiffForFile('package.json').then((diff) => {
+  // if (diff.dependencies) {
+
+  // }
+  console.log('Diff:', diff)
+})
