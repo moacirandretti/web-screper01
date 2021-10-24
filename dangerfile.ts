1428391c82b097e0b604c14cd58a3e97b3ed4d4c
moacirandretti <moacir.andretti@gmail.com>
2021-10-24 00:19:30 -0300

    chore: altera dangerfile

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,14 +1,19 @@
-import { danger, fail, warn, message, markdown } from 'danger'
+import { danger, fail, warn, message, markdown, schedule } from 'danger'
+import noConsole from 'danger-plugin-no-console'
+
+// Note: You need to use schedule()
+schedule(noConsole())
 
 const routeFile = danger.git.fileMatch('src/routes.ts')
 
 if (routeFile.edited) {
-  fail('Você alterou o arquivo de rotas')
+  warn('Você alterou o arquivo de rotas')
 }
 
 danger.git.JSONDiffForFile('package.json').then((diff) => {
-  // if (diff.dependencies) {
-
-  // }
   console.log('Diff:', diff)
+  if (diff.dependencies?.added.length > 0) {
+    const depName = diff.dependencies?.added.map(item => item)
+    warn(`Opaa!!! Você adicionou os seguintes pacotes:\n ${depName} \nCuidado com o tamanho do bundle! :)`)
+  }
 })
