24106eaf38e13ad3de56e1242731ef24b3a0524e
moacirandretti <moacir.andretti@gmail.com>
2021-10-23 23:01:27 -0300

    chore: adiciona dangerfile

---
+++ dangerfile.ts
@@ -0,0 +1,7 @@
+import { danger, fail, warn, message, markdown } from 'danger'
+
+const routeFile = danger.git.fileMatch('src/routes.ts')
+
+if (!routeFile.edited) {
+  message('Você não adicionou nenhum endpoint nesse commit!')
+}
