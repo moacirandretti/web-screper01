b47b005ab661eb6cd3d0a2c9705fa1d36aa90857
moacirandretti <moacir.andretti@gmail.com>
2021-10-23 23:09:14 -0300

    chore: altera dangerfile

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,5 +3,5 @@ import { danger, fail, warn, message, markdown } from 'danger'
 const routeFile = danger.git.fileMatch('src/routes.ts')
 
 if (routeFile.edited) {
-  message('Você não adicionou nenhum endpoint nesse commit!')
+  fail('Você alterou o arquivo de rotas')
 }
