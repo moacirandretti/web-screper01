b13b14cd89fd11dba9e1eb2d74a524193d55ebbf
moacirandretti <moacir.andretti@gmail.com>
2021-10-26 20:47:48 -0300

    chore: teste de console.log 4

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,36 +1,37 @@
 import { danger, warn } from 'danger'
+import noConsole from 'danger-plugin-no-console'
 
-// Note: You need to use schedule()
+noConsole()
 
 const routeFile = danger.git.fileMatch('src/routes.ts')
 
-const diffs = danger.git.created_files
-  .concat(danger.git.modified_files)
-  .map(file => {
-    return danger.git.diffForFile(file).then(diff => ({
-      file,
-      diff
-    }))
-  })
-const findConsole = (content: any) => {
-  const PATTERN = /console\.(log|error|warn|info)/
-  const GLOBAL_PATTERN = new RegExp(PATTERN.source, 'g')
-  let matches = content.match(GLOBAL_PATTERN)
-  if (!matches) return []
+// const diffs = danger.git.created_files
+//   .concat(danger.git.modified_files)
+//   .map(file => {
+//     return danger.git.diffForFile(file).then(diff => ({
+//       file,
+//       diff
+//     }))
+//   })
+// const findConsole = (content: any) => {
+//   const PATTERN = /console\.(log|error|warn|info)/
+//   const GLOBAL_PATTERN = new RegExp(PATTERN.source, 'g')
+//   let matches = content.match(GLOBAL_PATTERN)
+//   if (!matches) return []
 
-  matches = matches.filter(match => {
-    const singleMatch = PATTERN.exec(match)
-    if (!singleMatch || singleMatch.length === 0) return false
-  })
+//   matches = matches.filter(match => {
+//     const singleMatch = PATTERN.exec(match)
+//     if (!singleMatch || singleMatch.length === 0) return false
+//   })
 
-  return matches
-}
-console.log('>>console1')
-console.log('>>matches', findConsole(diffs))
+//   return matches
+// }
+// console.log('>>console1')
+// console.log('>>matches', findConsole(diffs))
 
-if (findConsole(diffs)) {
-  warn('Você abandonou um console.log! :( ')
-}
+// if (findConsole(diffs)) {
+//   warn('Você abandonou um console.log! :( ')
+// }
 
 if (routeFile.edited) {
   warn('Você alterou o arquivo de rotas')
